a98e88564e685d908de5c96fce4405b4c8303c19
Abdullah-hanini <98618536+Abdullah-hanini@users.noreply.github.com>
2023-08-17 12:58:52 +0300

    more ignored words

--- Profile/models.py
+++ Profile/models.py
@@ -5,6 +5,8 @@ from groups.models import group
 class user_data (models.Model):
     user = models.OneToOneField(new_user, on_delete=models.CASCADE)
     groups = models.ManyToManyField(group, related_name='groups', blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    bio = models.TextField(max_length=500, blank=True)
     
     def __str__(self):
         return self.user
