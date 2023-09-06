1df2053851790200beae3870e9a32e6793695d91
Abdullah-hanini <98618536+Abdullah-hanini@users.noreply.github.com>
2023-09-06 23:18:35 +0300

    casc

---
+++ static/assets/js/script.js
@@ -0,0 +1,9 @@
+
+var telInput = $("#phone");
+telInput.intlTelInput();
+$('#signInContainer').hide();
+$('#logInLink').click(function(){
+$('.signUpContainer').hide();
+$('#signInContainer').show();
+
+});
